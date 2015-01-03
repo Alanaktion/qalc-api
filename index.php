df5de78d5304fc3819f1fb49a80dc5960b56a989
Alan Hardman <alanaktion@gmail.com>
2015-01-02 19:45:56 -0700

    Use terse output

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ $response = array("error" => 0, "result" => "");
 
 if(isset($_REQUEST['q']) && $_REQUEST['q'] != '') {
 	$arg = escapeshellarg($_REQUEST['q']);
-	$result = shell_exec("/usr/bin/qalc $arg");
+	$result = shell_exec("/usr/bin/qalc -t $arg");
 	$response["result"] = trim($result);
 } else {
 	$response["error"] = "No expression specified, parameter `q` is missing or empty.";
