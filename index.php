184532da4666a1f6a857c75f509995861679f566
Alan Hardman <alanaktion@gmail.com>
2015-01-02 19:55:40 -0700

    Who needs JSON anyway? :P

--- index.php
+++ index.php
@@ -1,14 +1,9 @@
 <?php
 
-$response = array("error" => 0, "result" => "");
-
 if(isset($_REQUEST['q']) && $_REQUEST['q'] != '') {
 	$arg = escapeshellarg($_REQUEST['q']);
 	$result = shell_exec("/usr/bin/qalc -t $arg");
-	$response["result"] = trim($result);
+	echo trim($result);
 } else {
-	$response["error"] = "No expression specified, parameter `q` is missing or empty.";
+	echo "Error: no expression.";
 }
-
-header("Content-type: application/json");
-echo json_encode($response);
